69050760e0823f5aa1be4385b37a632318d9ccfd
Stijn Lingmont <stijnlingmont@hotmail.com>
2024-03-04 11:57:59 +0100

    Initial Commit

---
+++ CookieClicker/CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieClickerApp.swift
+//  CookieClicker
+//
+//  Created by Stijn Lingmont on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
