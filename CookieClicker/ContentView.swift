e2c2033dba83e57884f606dd0c82ec449b03084d
Stijn Lingmont <stijnlingmont@hotmail.com>
2024-03-04 12:59:54 +0100

    Base cookie clicker

--- CookieClicker/ContentView.swift
+++ CookieClicker/ContentView.swift
@@ -1,24 +1,61 @@
-//
-//  ContentView.swift
-//  CookieClicker
-//
-//  Created by Stijn Lingmont on 04/03/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @Binding var amountClicked : Double
+    @Binding var autoClickers : Int
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CookieIncreaser
+            Upgrades
+        }
+    }
+    
+    var CookieIncreaser: some View {
+        VStack {
+            Text("Cookie Clicker")
+            
+            VStack {
+                Button(action: { amountClicked += 1 }) {
+                    if let image = UIImage(named: "Cookie") {
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300, height: 300)
+                            .padding()
+                    } else {
+                        // Handle the case when the image cannot be loaded
+                        Text("Image not found.")
+                    }
+                }
+            }
+            .padding()
+            
+            if amountClicked > 0 {
+                Text("Cookies collected: \(amountClicked)")
+            } else {
+                Text("Click the cookie!")
+            }
+            
         }
         .padding()
     }
+    
+    func buyAutoClicker() {
+        if amountClicked >= 10 {
+            autoClickers += 1
+            amountClicked -= 10
+        }
+    }
+    
+    var Upgrades: some View {
+        Button(action: buyAutoClicker ) {
+            Text("Buy auto clicker (10 cookies)")
+        }
+    }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    var amountClicked = 0
+//    ContentView(amountClicked: amountClicked)
+//}
--- CookieClicker/CookieClickerApp.swift
+++ CookieClicker/CookieClickerApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct CookieClickerApp: App {
+    @State var amountClicked: Double = 0.0
+    @State var autoClickers = 0
+    @State private var timer: Timer?
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(amountClicked: $amountClicked, autoClickers: $autoClickers)
+                .onAppear {
+                    startTimer()
+                }
+                .onDisappear {
+                    stopTimer()
+                }
         }
+    }
+
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            amountClicked += (Double(autoClickers) * 0.1)
+        }
+    }
+
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
     }
 }
